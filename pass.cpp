a468f0bebce650a21bc07bbe882b2ca7a158d7b9
msmol <msmolash@shutterstock.com>
2017-06-05 10:16:27 -0400

    Make the pass otp identifier an environment variable

--- pass.cpp
+++ pass.cpp
@@ -125,7 +125,13 @@ void Pass::run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &m
 {
     Q_UNUSED(context);
 
-    auto ret = match.text().contains("totp-", Qt::CaseInsensitive) ?
+    this->passOtpIdentifier = "totp::";
+    auto passOtpIdentifier = getenv("PASSWORD_STORE_OTP_IDENTIFIER");
+    if (passOtpIdentifier != nullptr) {
+        this->passOtpIdentifier = passOtpIdentifier;
+    }
+
+    auto ret = match.text().contains(this->passOtpIdentifier, Qt::CaseInsensitive) ?
         QProcess::execute(QString("pass otp -c ") + match.text()) :
         QProcess::execute(QString("pass -c ") + match.text());
     if (ret == 0) {
